0e30876dc0cc96749e7f7cf3029612c94bf6970a
Zaenal <aenal.abie@gmail.com>
2016-10-29 11:38:56 +0700

    pengecekan number valid

--- libs/world.prime.factors/prime.factors.js
+++ libs/world.prime.factors/prime.factors.js
@@ -3,16 +3,19 @@ var primeFactorsOf = require('./lib/prime.factors.of');
 var primeFactors = function(request, response) {
 
 	var number = parseInt(request.query['number']);
+	var pesan;
 	var decomposition;
 	if(Number.isInteger(number)){
 			decomposition = primeFactorsOf(number);
+			pesan = { number: number, decomposition: decomposition };
 	} else {
 			decomposition = request.query['number'];
+			pesan  = {number:decomposition , "error" : "not a number"};
 	}
 
 
 	response.setHeader('Content-Type', 'application/json');
-    response.send({ number: number, decomposition: decomposition });
+    response.send(pesan);
 };
 
 module.exports = primeFactors;
