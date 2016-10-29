a8e1a7bbbf2afeae33a0e371799d0ff2697e8988
Zaenal <aenal.abie@gmail.com>
2016-10-29 11:05:05 +0700

    new prime lib

--- libs/world.prime.factors/lib/prime.factors.of.js
+++ libs/world.prime.factors/lib/prime.factors.of.js
@@ -1,7 +1,7 @@
 primeFactorsOf = function(number) {
 	var factors = [];
 	var fac=2;
-	while (number>2) {
+	while (number>=2) {
 		while (number%fac===0) {
 			factors.push(fac);
 			number = number / fac;
